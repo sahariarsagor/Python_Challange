e0ba1bc26aa24ca3637b3edfea252b8743f82327
sahariarsagor <contactsahariar@gmail.com>
2026-01-01 10:31:33 +0600

    problem 03 which is to find the maximum of two number

---
+++ Beginner Level/problem_03.py
@@ -0,0 +1,10 @@
+# find the maximum of two numbers
+
+num1 = int(input("Enter number 01: "))
+num2 = int(input("Enter number 02: "))
+
+if num1 > num2:
+    print("Number 1 is greater than number 2")
+else:
+    print("Number 02 is greater than number 01")
+
