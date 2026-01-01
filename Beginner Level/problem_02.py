39171f842b400fd6fde61b6ab14a306d7bfc9bbf
sahariarsagor <contactsahariar@gmail.com>
2026-01-01 10:27:19 +0600

    problem 02 of find the sum of two number

---
+++ Beginner Level/problem_02.py
@@ -0,0 +1,7 @@
+#print the sum of two numbers
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter Second Number: "))
+
+sum = num1 + num2
+print("The Summation of these number is: ", sum)
